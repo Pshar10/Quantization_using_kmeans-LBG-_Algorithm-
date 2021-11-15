79b05dfbacc8b8e27c258c1889a38cbfdbbd1771
Pranav <89517077+Pshar10@users.noreply.github.com>
2021-11-15 13:59:36 +0100

    Add files via upload

--- Seminar_2/Uniform_Quantization.py
+++ Seminar_2/Uniform_Quantization.py
@@ -14,6 +14,8 @@ rate1, data = wav.read("Track48.wav")
 test_signal = data[:,1]  
 pickle.dump(test_signal,open("test_signal.bin","wb"),1)  
 
+test_signal = test_signal/np.max(test_signal)
+
 
 samplerate, speech = wav.read("speech.wav")
 speech = m.clipaudio(speech,samplerate,1,3)
@@ -23,16 +25,19 @@ data = speech
 
 
 
-q =int((np.max(test_signal))- (np.min(test_signal))/(2**n))    #stepsize
-index = np.round(test_signal/q)    # mid-tread
-mid_tread_quantized = index*q
+qu =((np.max(test_signal))- (np.min(test_signal)))    #stepsize
+step_size = qu/(2**n)
+index = np.round(test_signal/step_size)    # mid-tread
+mid_tread_quantized = index*step_size
+
+
 
 l1, = plt.plot(test_signal, color= 'red')
 l2, = plt.plot(mid_tread_quantized, color= 'green')
 plt.title('comparison of reconstructed signal')
 plt.xlabel('samples')
 plt.ylabel('Magnitude')
-plt.legend(handles = [l1,l2,],labels = ['original','mid-tread'])
+plt.legend(handles = [l2,l1],labels = ['original','mid-tread'])
 plt.show()
 
 #index = index.astype('int8')
